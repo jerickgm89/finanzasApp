bcd8901dd72bb946421f8f03e9e6bd05b28ef46e
JErickDev <jerickgm89@gmail.com>
2024-07-17 22:44:14 -0500

    Config: configuracion inicial del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Typography } from '@mui/material'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => (
+  <>
+    <Typography 
+      sx={{
+        fontSize: 100,
+        textAlign: 'center',
+      }}
+    >
+      text
+    </Typography>
+    <Typography 
+      fontSize={50}
+      align='center'
+    >
+      text2
+    </Typography>
+    <Button variant="contained" 
+      sx={{
+        backgroundColor: "#1e1e2e",
+        '&:hover': {
+      backgroundColor: "#3e3e5e", // Cambia este color por el que desees para el hover
+    },
+      }} >
+        Contained
+    </Button>
+  </>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+)
